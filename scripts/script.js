9337a02b51d9cfba92b6bd9574821250ad586546
AntonUrievi4 <An_t_o_n@rambler.ru>
2019-07-29 13:19:16 +0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var one = 1984;
+alert(one);
+var two = "TWO, two, TwO, tWo";
+var three = true;
+var four = null;
+var five = undefined;
+console.log(one, two, three, four, five)
+console.log(typeof one, two, three, four, five)
+var pro = prompt("Число");
+console.log("Квадрат = " + pro*pro);
