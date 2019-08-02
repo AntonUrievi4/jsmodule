185a7ac5c963cb54083faa3907008be40c8b58a5
AntonUrievi4 <An_t_o_n@rambler.ru>
2019-08-02 14:17:36 +0400

    correcting code

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,11 @@ var three = true;
 var four = null;
 var five = undefined;
 
-console.log("one =" + " " + one + " " + "type" + " " + typeof one);
-console.log("two =" + " " + two + " " + "type" + " " + typeof two);
-console.log("three =" + " " + three + " " + "type" + " " + typeof three);
-console.log("four =" + " " + four + " " + "type" + " " + typeof four);
-console.log("five =" + " " + five + " " + "type" + " " + typeof five);
+console.log("one = " + one + " type " + typeof one);
+console.log("two = " + two + " type "  + typeof two);
+console.log("three = " + three + " type " + typeof three);
+console.log("four = " + four + " type " + typeof four);
+console.log("five = " + five + " type " + typeof five);
 
 var pro = prompt("Квадрат числа", "Введите число");
 alert("Квадрат = " + pro*pro);
