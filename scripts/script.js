41280d4963adbdbb56f0fa51f1e2cc5640b5bd0d
AntonUrievi4 <An_t_o_n@rambler.ru>
2019-08-02 15:48:36 +0400

    add comparison letters

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,15 @@ var nuber_one = prompt("Первое число");
 var number_two = prompt("Второе число");
 
 if (nuber_one > number_two) {
-  alert("Большее число" + nuber_one)
+  alert("Большее число = " + nuber_one)
 } else {
   alert("Большее число = " + number_two)
+}
+var letter_one = prompt("Введите первую букву");
+var letter_two = prompt("Введите вторую букву");
+
+if (letter_one < letter_two) {
+    alert("Буква " + letter_one + " стоит раньше " + letter_two)
+} else {
+    alert("Буква " + letter_two + " стоит раньше " + letter_one)
 }
