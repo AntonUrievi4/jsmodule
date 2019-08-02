0c23a8f71aba232a318367ee494c15dc3f080cdb
AntonUrievi4 <An_t_o_n@rambler.ru>
2019-08-02 17:19:26 +0400

    add comments about tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//Первое задание первая часть по выводу содержимого каждой переменной и типа каждой переменной
+
 var one = 1984;
 var two = "Second var";
 var three = true;
@@ -10,8 +12,14 @@ console.log("three = " + three + " type " + typeof three);
 console.log("four = " + four + " type " + typeof four);
 console.log("five = " + five + " type " + typeof five);
 
+//Первое задание Вторая часть по созданию скрипта вывода квадрата числа
+
 var pro = prompt("Квадрат числа", "Введите число");
 alert("Квадрат = " + pro*pro);
+
+//Второе задание первая часть
+
+//Второе задание вторая часть по выводу наибольшего числа
 
 var nuber_one = prompt("Первое число");
 var number_two = prompt("Второе число");
@@ -21,6 +29,9 @@ if (nuber_one > number_two) {
 } else {
   alert("Большее число = " + number_two)
 }
+
+//Второе задание третья часть по выводу букв алфавита
+
 var letter_one = prompt("Введите первую букву");
 var letter_two = prompt("Введите вторую букву");
 
