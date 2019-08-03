231eef40002b978102d5a250b8e4140331791f4c
AntonUrievi4 <An_t_o_n@rambler.ru>
2019-08-03 12:21:21 +0400

    add task 3.1; 3.2.1; 3.2.2; 3.3

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//Первое задание первая часть по выводу содержимого каждой переменной и типа каждой переменной
+/* 1.1 по выводу содержимого каждой переменной и типа каждой переменной
 
 var one = 1984;
 var two = "Second var";
@@ -12,18 +12,18 @@ console.log("three = " + three + " type " + typeof three);
 console.log("four = " + four + " type " + typeof four);
 console.log("five = " + five + " type " + typeof five);
 
-//Первое задание Вторая часть по созданию скрипта вывода квадрата числа
+// 1.2 по созданию скрипта вывода квадрата числа
 
 var pro = prompt("Квадрат числа", "Введите число");
 alert("Квадрат = " + pro*pro);
 
-//Второе задание первая часть сравнение разных типов данных с выводом сравнения и пояснением
+// 2.1 сравнение разных типов данных с выводом сравнения и пояснением
 
 var type_one = prompt("ВВедите первые данные");
 var type_two = prompt("ВВедите вторые данные");
 alert( type_one == type_two);
 
-//Второе задание вторая часть по выводу наибольшего числа
+// 2.2 по выводу наибольшего числа
 
 var nuber_one = prompt("Первое число");
 var number_two = prompt("Второе число");
@@ -34,7 +34,7 @@ if (nuber_one > number_two) {
   alert("Большее число = " + number_two)
 }
 
-//Второе задание третья часть по выводу букв алфавита
+//2.3 по выводу букв алфавита
 
 var letter_one = prompt("Введите первую букву");
 var letter_two = prompt("Введите вторую букву");
@@ -44,3 +44,44 @@ if (letter_one < letter_two) {
 } else {
     alert("Буква " + letter_two + " стоит раньше " + letter_one)
 }
+*/
+
+// 3.1
+
+var user_name = prompt("Как вас зовут?");
+var user_age = prompt("Сколько вам лет?")
+
+var user = {
+  name: user_name,
+  age: user_age
+}
+
+console.log(user);
+
+// 3.2.1
+
+var colors = [];
+
+for (var i = 4; i >= 0; i--) {
+  colors[i] = prompt('Напишите цвет');
+}
+console.log(colors);
+
+// 3.2.2
+
+var colors = [];
+var i = 4;
+while ( i >= 0 ) {
+  colors[i] = prompt('Напишите цвет');
+  i--;
+}
+console.log(colors);
+
+// 3.3
+
+var mag = {};
+
+for (var i = prompt('Количество товара'); i > 0; i--) {
+  mag[i] = prompt("наименование");
+}
+console.log(mag);
